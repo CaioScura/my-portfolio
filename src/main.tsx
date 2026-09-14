58d8b2da6e2b24f3e450cea8d8ead2f1f0a45dc7
CaioScura <caioscura02@gmail.com>
2026-09-14 17:31:17 -0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual'
+}
+window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
