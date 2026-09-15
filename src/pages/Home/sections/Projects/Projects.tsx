de464ff786dc39f0d37898c48af3c20bffa7094c
CaioScura <caioscura02@gmail.com>
2026-09-15 17:46:11 -0300

    add projetos

--- src/pages/Home/sections/Projects/Projects.tsx
+++ src/pages/Home/sections/Projects/Projects.tsx
@@ -36,11 +36,26 @@ const projects: ProjectCardProps[] = [
         liveUrl: 'https://movies-app-caiote.vercel.app/',
     },
     {
+        category: 'Front-end',
+        title: 'Extensão Gatos Flutuantes',
+        description: 'Extensão para navegador que adiciona gatinhos animados em qualquer página da web. Possui diversas animações que eu mesmo criei utilizando software de pixel art, você pode mover os gatos e deixar em qualquer canto da tela.',
+        stack: ['JavaScript', 'HTML', 'CSS', 'Manifest V3'],
+        image: img('img-extensao-gatos.png'),
+        repoUrl: 'https://github.com/CaioScura/petExtension'
+    },
+    {
         category: 'Back-end',
         title: 'Sistema de Gestão de Fornecedores',
         description: 'Sistema desenvolvido em Java para gerenciamento de fornecedores, com operações de cadastro, alteração, exclusão e consulta. A aplicação utiliza JDBC para conexão com o banco de dados e uma interface gráfica para interação com o usuário.',
         stack: ['Java', 'JDBC', 'SQL', 'POO', 'MySQL'],
         repoUrl: 'https://github.com/CaioScura/prjJavaFornecedoresJDBC'
+    },
+    {
+        category: 'Back-end',
+        title: 'API REST em Java',
+        description: 'API REST desenvolvida com Spring Boot para gerenciamento de médicos e pacientes de uma clínica médica.',
+        stack: ['Java', 'SpringBoot', 'MySQL'],
+        repoUrl: 'https://github.com/CaioScura/API-REST-springboot'
     },
 ]
 
